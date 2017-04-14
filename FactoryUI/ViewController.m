e8e66c9febba7f37fae98d5ff083b988bcdbfc05
范云飞 <786452470@qq.com>
2017-04-14 10:52:40 +0800

    Initial Commit

---
+++ FactoryUI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FactoryUI
+//
+//  Created by 范云飞 on 2017/4/14.
+//  Copyright © 2017年 范云飞. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
